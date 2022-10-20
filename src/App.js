7a267fe783befbae3dab783db58febae81ab115e
조수영 <josuyong96@gmail.com>
2022-10-20 17:45:29 +0900

    useEffect 한번만 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [cnt, setCnt] = useState(0);
+  const onClick = () => setCnt((prev) => prev + 1);
+  console.log("i run all the time")
+  const iRun = () => {
+    console.log("i run only once.");
+  }
+  useEffect(iRun, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
+      <h1 className={styles.title}>{cnt}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
